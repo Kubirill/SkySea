fe5a6ec685c95528fa44843990d1cb9578aa35ba
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-07 02:07:32 +0300

    feat: :sparkles: cloud river

--- Assets/environment/Scripts/button/Moveplot.cs
+++ Assets/environment/Scripts/button/Moveplot.cs
@@ -14,12 +14,12 @@ public class Moveplot : MonoBehaviour {
             centerButton ();
     }
     void leftButton () =>
-        pet.transform.position = new Vector3 (LeftB.position.x, LeftB.position.y + 5, LeftB.position.z);
+        pet.transform.position = new Vector3 (LeftB.position.x, LeftB.position.y + 1, LeftB.position.z);
 
     void rightButton () =>
-        pet.transform.position = new Vector3 (RightB.position.x, RightB.position.y + 5, RightB.position.z);
+        pet.transform.position = new Vector3 (RightB.position.x, RightB.position.y + 1, RightB.position.z);
 
     void centerButton () =>
-        pet.transform.position = new Vector3 (center.position.x, center.position.y + 5, center.position.z);
+        pet.transform.position = new Vector3 (center.position.x, center.position.y + 1, center.position.z);
 
 }
