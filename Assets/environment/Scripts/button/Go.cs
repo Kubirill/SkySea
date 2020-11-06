16aa890a7ecfdc325227fd6fb1c3740e60c4ec5d
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-07 00:52:25 +0300

    add smooth move on start + lighting bolt

--- Assets/environment/Scripts/button/Go.cs
+++ Assets/environment/Scripts/button/Go.cs
@@ -1,9 +1,31 @@
-﻿using DG.Tweening;
+﻿using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Go : MonoBehaviour {
     [SerializeField] Transform hero;
-    [SerializeField] float speed = 20;
-    public void OnTriggerStay () =>
-        hero.Translate (hero.forward * speed * Time.deltaTime, Space.World);
+    [SerializeField] float speed = 0;
+    [SerializeField] float maxspeed = 20;
+    Transform buff;
+    bool afterGo = false;
+    public void OnTriggerStay () => move ();
+    // smooth for start
+    private void OnTriggerEnter (Collider other) => DOTween.To (() => speed, x => speed = x, maxspeed, 1);
+    // smooth for end
+    private void OnTriggerExit () {
+        DOTween.To (() => speed, x => speed = x, 0, 1);
+        afterGo = true;
+        StartCoroutine (AfterMove ());
+    }
+    IEnumerator AfterMove () {
+        yield return new WaitForSeconds (1f);
+        afterGo = false;
+    }
+    private void FixedUpdate () {
+        if (afterGo)
+            move ();
+        else return;
+    }
+    // move object
+    void move () => hero.Translate (hero.forward * speed * Time.deltaTime, Space.World);
 }
---
+++ Assets/environment/Scripts/controlCloudSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+public class controlCloudSound : MonoBehaviour {
+
+    [SerializeField] GameObject cloud;
+    [SerializeField] GameObject light;
+    AudioSource _audioSource;
+    private void Start () => _audioSource = cloud.GetComponent<AudioSource> ();
+    private void OnTriggerEnter () {
+        _audioSource.Play ();
+        light.SetActive (true);
+        StartCoroutine (TestCoroutine ());
+    }
+    IEnumerator TestCoroutine () {
+        yield return new WaitForSeconds (5f);
+        light.SetActive (false);
+    }
+}
