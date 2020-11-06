5e93fd6e72bc073ec9b0dd2e09b06b81311a04cc
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-06 19:32:19 +0300

    fix go

--- Assets/environment/Scripts/button/Go.cs
+++ Assets/environment/Scripts/button/Go.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 
 public class Go : MonoBehaviour {
     [SerializeField] Transform hero;
-    [SerializeField] float speed = 1;
-    public void OnTriggerStay () {
-        // hero.Translate (this.transform.forward * 1 * Time.deltaTime, Space.World);
-        // hero.DOMove (this.transform.forward, speed);
-        hero.Translate (hero.forward * 20 * Time.deltaTime);
-        // print ($"10. Go -> this.transform.forward : {this.transform.forward}");
-    }
+    [SerializeField] float speed = 20;
+    public void OnTriggerStay () =>
+        hero.Translate (hero.forward * speed * Time.deltaTime, Space.World);
 }
--- Assets/environment/Scripts/button/Rotate.cs
+++ Assets/environment/Scripts/button/Rotate.cs
@@ -9,10 +9,7 @@ public class Rotate : MonoBehaviour {
 	float rotAngle = 0;
 
 	private void OnTriggerEnter () {
-		// string msg = "Turn on " + (left ? "left" : "right") + $" with {angle}";
-		// print (msg);
-		rotAngle = left ? -15 : 15;
-		print ($"15. Rotate -> rotAngle : {rotAngle}");
+		rotAngle += left ? -angle : angle;
 		plot.DORotate (new Vector3 (0, rotAngle, 0), smooth);
 	}
 }
