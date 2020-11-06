f8bbac5e167ea4df0822500a4f1aa4cf6d0a1d53
Kubirill <Kubirill@rambler.ru>
2020-11-06 19:31:47 +0300

    Add lazer

---
+++ Assets/Kubirill/Scripts/Lazer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour
+{
+    public GameObject lazerPoint;
+    public float MaxDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit point;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out point,MaxDistance))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * point.distance, Color.red);
+            lazerPoint.transform.position = new Vector3(point.point.x, point.point.y, point.point.z);
+            lazerPoint.transform.up = point.normal;
+        }
+    }
+}
