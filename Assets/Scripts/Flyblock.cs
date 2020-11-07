c070422b114b24341288fca795c9cae0067cce6c
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-07 18:52:37 +0300

    add 2 task

---
+++ Assets/Scripts/Flyblock.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using PathCreation.Examples;
+using UnityEngine;
+public class Flyblock : MonoBehaviour {
+
+    float oldHeight;
+    [SerializeField] PathFollower path;
+    bool fly = false;
+    private void Start () {
+        path = this.gameObject.GetComponent<PathFollower> ();
+    }
+    public void go () {
+        path.enabled = true;
+    }
+
+    private void FixedUpdate () {
+        if (Input.GetKeyDown (KeyCode.Q))
+            go ();
+
+    }
+
+}
