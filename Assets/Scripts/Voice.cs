e55c2eec13378beee70e6dc59a3effd9903fc030
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-07 13:10:54 +0300

    dragon voice

---
+++ Assets/Scripts/Voice.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Voice : MonoBehaviour {
+
+	[SerializeField] AudioClip[] _voiceClips;
+	AudioSource _audioSource = null;
+	void Start () {
+		_audioSource = this.gameObject.GetComponent<AudioSource> ();
+		PlayNextSong ();
+	}
+	//Play a list of voices in random order
+	void PlayNextSong () {
+		PlayVoice ();
+		Invoke ("PlayNextSong", _audioSource.clip.length + Random.Range (3, 30));
+	}
+	public void PlayVoice () {
+		if (_audioSource != null) {
+			_audioSource.clip = _voiceClips[Random.Range (0, _voiceClips.Length)];
+			_audioSource.Play ();
+		}
+
+	}
+}
