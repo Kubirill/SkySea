9f0819a3276b3a3cd77c5e53407f004000fa9e9b
Kubirill <Kubirill@rambler.ru>
2020-11-07 10:02:56 +0300

    Add food for dragon

--- Assets/Kubirill/Scripts/DragonMove.cs
+++ Assets/Kubirill/Scripts/DragonMove.cs
@@ -5,42 +5,56 @@ using UnityEngine;
 
 public class DragonMove : MonoBehaviour
 {
+    Vector3 start;
     Rigidbody rb;
     Animator anim;
     string playing = "deeze";
     public GameObject target;
+    public GameObject targetLazer;
+    Vector3 addTarget = Vector3.zero;
     public Transform player;
     public float duration;
     public float jump;
     public float jumpDuration;
+    public float limitY;
     public int num;
     bool isJumping=false;
+    public bool isFood=false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        start = transform.localPosition;
+        if (target == null) target = targetLazer;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if(anim.GetCurrentAnimatorStateInfo(0).IsName("dizzy"))
+        if (isFood&& Vector3.Distance(transform.position, target.transform.position)>15)
+        {
+            target = targetLazer;
+            isFood = false;
+        }
+        if (target==targetLazer) isFood = false;
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("dizzy"))
         {
             transform.DOLookAt(player.position, duration/5);
-            rb.useGravity = true;
+            //rb.useGravity = true;
         }
         if (target.activeInHierarchy)
         {
-            if ((Vector3.Distance(target.transform.position - new Vector3(0, target.transform.position.y, 0), transform.position - new Vector3(0, transform.position.y, 0)) > 0.5f)) anim.SetBool("Finish", false);
+            if ((Vector3.Distance(target.transform.position-new Vector3(0, target.transform.position.y, 0) , transform.position - new Vector3(0, transform.position.y, 0))  > 1f)) anim.SetBool("Finish", false);
             else
             {
 
                 if (!anim.GetBool("Finish"))
                 {
-                    transform.DOLookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z), 0.1f);
-
+                    //transform.DOLookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z), 0.1f);
+                    rb.useGravity = true;
+                    rb.velocity = Vector3.zero;
                     transform.transform.localPosition= new Vector3(target.transform.localPosition.x, transform.localPosition.y, target.transform.localPosition.z);
                     transform.transform.DOPause();
                 }
@@ -61,18 +75,50 @@ public class DragonMove : MonoBehaviour
         }
         if (!anim.GetBool("Finish"))
         {
-            Vector3 addTarget = Vector3.zero;
-            if (Vector3.Distance(target.transform.up, Vector3.up) < 0.2) addTarget = new Vector3(0, 1.8f, 0);
+            addTarget = Vector3.zero;
+            if (Vector3.Distance(target.transform.up, Vector3.up) < 0.2) addTarget = new Vector3(0, 1f, 0);
 
 
             rb.useGravity = false;
             transform.DOLookAt(target.transform.position + addTarget, duration / 5);
             if (anim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
             {
-                 transform.DOLocalMove(target.transform.localPosition + addTarget, duration);
-
+                transform.DOLocalMove(target.transform.localPosition + addTarget, duration - (isFood ? 1 : 0));
+                Debug.Log(target.transform.localPosition + addTarget);
             }
         }
 
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "food")
+        {
+            target = other.gameObject;
+            anim.SetTrigger("Food");
+        }
+    }
+    /*public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "food")
+        {
+            target = collision.gameObject;
+            anim.SetTrigger("Food");
+        }
+    }*/
+    public void Eat()
+    {
+        if (isFood)
+        {
+            GameObject.Destroy(target);
+            target = targetLazer;
+            isFood = false;
+        }
+        
+    }
+    public void LateUpdate()
+    {
+        if (isFood) transform.DOLookAt(target.transform.position , duration / 5);
+        if (transform.position.y < limitY) transform.position = start;
     }
 }
