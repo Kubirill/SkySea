cb32f2077b78c664f5ee5f9f65c102849cfc20de
Kubirill <Kubirill@rambler.ru>
2020-12-06 18:08:17 +0300

    Finish demo level1

--- Assets/Kubirill/Scripts/Level1.cs
+++ Assets/Kubirill/Scripts/Level1.cs
@@ -1,15 +1,49 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Level1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public buttonLevel1[] buttoms;
+    public int[] correctNum;
+    int step=0;
+    public Transform lattice;
+    public Transform targetLattice;
+    public float speedLattice =0.5f;
     void Start()
     {
-        
+        foreach (buttonLevel1 buttom in buttoms)
+        {
+            buttom.SetMain(GetComponent<Level1>());
+        }
     }
 
+    public void Press (int num)
+    {
+        if (correctNum[step]==num)
+        {
+            buttoms[num].SetMaterial("correct");
+            if (step < correctNum.Length-1)
+            {
+                step++;
+                
+            }
+            else
+            {
+                lattice.DOMove(targetLattice.position,speedLattice);
+            }
+
+        }
+        else
+        {
+            step = 0;
+            foreach (buttonLevel1 buttom in buttoms)
+            {
+                buttom.SetMaterial("unactive");
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Kubirill/Scripts/buttonLevel1.cs
+++ Assets/Kubirill/Scripts/buttonLevel1.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class buttonLevel1 : MonoBehaviour
 {
+    Level1 mainScript;
+    public int number;
+    public Material wrong;
+    public Material correct;
+    public Material unactive;
     // Start is called before the first frame update
-    void Start()
+    public void OnCollisionEnter(Collision collision)
     {
-        
+        if ((collision.gameObject.tag == "Dragon")&&(gameObject.GetComponent<MeshRenderer>().material!= correct))
+        {
+            
+
+            mainScript.Press(number);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMain(Level1 scriptObj)
     {
-        
+        mainScript = scriptObj;
+    }
+    public void SetMaterial(string type)
+    {
+        switch (type)
+        {
+            case "wrong": 
+                gameObject.GetComponent<MeshRenderer>().material= wrong;
+                break;
+            case "correct": 
+                gameObject.GetComponent<MeshRenderer>().material = correct;
+                break;
+            default:
+                gameObject.GetComponent<MeshRenderer>().material = unactive;
+                break;
+        }
+
     }
 }
