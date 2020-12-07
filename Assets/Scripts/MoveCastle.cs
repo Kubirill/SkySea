4adaabd76033d533799829a65908e8341282bbd4
Dmitriy Makarov <Dimamakarov1941@yandx.ru>
2020-12-07 23:30:50 +0300

    make imagine from castle

---
+++ Assets/Scripts/MoveCastle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveCastle : MonoBehaviour {
+    [SerializeField] Transform player;
+    [SerializeField] int distance;
+    private void FixedUpdate () =>
+        this.transform.position = new Vector3 (
+            this.transform.position.x,
+            this.transform.position.y,
+            player.position.z + distance);
+}
