24c4fce98de16a985f123af4084a82181134ffde
Kubirill <Kubirill@rambler.ru>
2020-12-07 18:42:13 +0300

    Add demo level2

---
+++ Assets/Kubirill/Scripts/ButtomLevel2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtomLevel2 : MonoBehaviour
+{
+    public Transform leftGoal;
+    public Transform rightGoal;
+    public Transform leftOpen;
+    public Transform rightOpen;
+    public Transform leftClose;
+    public Transform rightClose;
+    public float speed;
+    bool stay=false;
+    bool isOpen = false;
+    public void OnTriggerStay(Collider other)
+    {
+        if ((other.tag == "Dragon") || (other.tag == "barrel"))
+        {
+            stay = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if ((other.tag == "Dragon") || (other.tag == "barrel"))
+        {
+            stay = false;
+        }
+    }
+    public void LateUpdate()
+    {
+        if ((!stay)&&(isOpen))
+        {
+            isOpen = false;
+            leftGoal.DORotateQuaternion(leftClose.rotation, speed);
+            rightGoal.DORotateQuaternion(rightClose.rotation, speed);
+        }
+        if ((stay) && (!isOpen))
+        {
+            isOpen = true;
+            leftGoal.DORotateQuaternion(leftOpen.rotation, speed);
+            rightGoal.DORotateQuaternion(rightOpen.rotation, speed);
+        }
+    }
+}
