1c1220c2d5ee57ba065ab6dd1cf767b550ec862f
Dmitriy Makarov <Dimamakarov1941@yandx.ru>
2020-12-13 17:51:05 +0300

    build ready

--- Assets/Scripts/MoveCastle.cs
+++ Assets/Scripts/MoveCastle.cs
@@ -3,9 +3,15 @@
 public class MoveCastle : MonoBehaviour {
     [SerializeField] Transform player;
     [SerializeField] int distance;
+
     private void FixedUpdate () =>
         this.transform.position = new Vector3 (
             this.transform.position.x,
             this.transform.position.y,
             player.position.z + distance);
+
+    // void LateUpdate () {
+    //     //block skinRotation
+    //     this.transform.forward = player.transform.forward;
+    // }
 }
