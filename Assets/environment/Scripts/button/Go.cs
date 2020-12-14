034dae95126c66523a7bb771c0d3cbb9792b6730
Dmitriy Makarov <Dimamakarov1941@yandx.ru>
2020-12-14 18:01:24 +0300

    add rigibody

--- Assets/environment/Scripts/button/Go.cs
+++ Assets/environment/Scripts/button/Go.cs
@@ -2,18 +2,30 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class Go : MonoBehaviour {
-    [SerializeField] Transform hero;
+public class Go : MonoBehaviour
+{
+    // move boat
+    [SerializeField] GameObject hero;
+    Transform heroTransform;
+    Rigidbody heroRigidbody;
+    [SerializeField] GameObject dragon;
+    Rigidbody dragonRigidbody;
+    Transform dragonTransform;
     [SerializeField] float speed = 0;
     [SerializeField] float maxspeed = 20;
-    Transform buff;
+    // Transform buff;
     bool afterGo = false;
 
     [SerializeField] AudioClip buttonSound;
     Animator anim;
     AudioSource _audioSource;
+
     private void Start()
     {
+        heroTransform = hero.GetComponent<Transform>();
+        heroRigidbody = hero.GetComponent<Rigidbody>();
+        dragonRigidbody = dragon.GetComponent<Rigidbody>();
+        dragonTransform = dragon.GetComponent<Transform>();
         _audioSource = this.gameObject.GetComponent<AudioSource>();
         anim = GetComponentInParent<Animator>();
     }
@@ -21,55 +33,73 @@ public class Go : MonoBehaviour {
     /*public void OnTriggerStay () => move ();*/
     public void OnTriggerStay(Collider other)
     {
+
         if (other.tag == "Dragon")
         {
             if (other.gameObject.GetComponent<DragonMove>().anim.GetBool("Finish"))
             {
                 if (!anim.GetBool("Press"))
                 {
-                   //smooth for start
-                    DOTween.To(() => speed, x => speed = x, maxspeed, 1);
+                    //smooth for start
+                    DOTween.To(() => speed, x => speed = x, maxspeed, 1); // to max speed, duration 1
                     anim.SetBool("Press", true);
+                    dragonRigidbody.isKinematic = true;
                     _audioSource.Play();
                 }
                 move();
             }
             else
             {
-                if (anim.GetBool("Press"))
-                {
-                    DOTween.To(() => speed, x => speed = x, 0, 1);
-                    afterGo = true;
-                    _audioSource.Play();
-                    StartCoroutine(AfterMove());
-                    anim.SetBool("Press", false);
-                }
+                SmoothEnd();
             }
         }
-        
+
     }
-    
+
     // smooth for end
-    private void OnTriggerExit (Collider other) {
-        if ((other.tag == "Dragon") &&( anim.GetBool("Press")))
+    void SmoothEnd()
+    {
+        if (anim.GetBool("Press"))
         {
-            DOTween.To(() => speed, x => speed = x, 0, 1);
+            // we stop go
+            DOTween.To(() => speed, x => speed = x, 0, 1); // to 0, duration 1
             afterGo = true;
             _audioSource.Play();
             StartCoroutine(AfterMove());
             anim.SetBool("Press", false);
         }
-            
     }
-    IEnumerator AfterMove () {
-        yield return new WaitForSeconds (1f);
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Dragon")
+        {
+            SmoothEnd();
+        }
+
+    }
+    IEnumerator AfterMove()
+    {
+        yield return new WaitForSeconds(1f);
         afterGo = false;
     }
-    private void FixedUpdate () {
+    private void FixedUpdate()
+    {
+        if (speed == 0 && dragonRigidbody.isKinematic)
+        {
+            dragonRigidbody.isKinematic = false;
+        }
         if (afterGo)
-            move ();
+            move();
         else return;
     }
-    // move object
-    void move () => hero.Translate (hero.forward * speed * Time.deltaTime, Space.World);
+    // move object with rigidbody
+    void move()
+    {
+        // Vector3 powerOfMove = new Vector3 ()
+        var powerOfMove = heroTransform.forward * speed; // set direction of movement (forward)
+        // without physics
+        // heroTransform.Translate (powerOfMove * Time.deltaTime, Space.World);
+        heroRigidbody.DOMove(heroTransform.position + powerOfMove, 1);
+    }
 }
+
--- Assets/environment/Scripts/button/Rotate.cs
+++ Assets/environment/Scripts/button/Rotate.cs
@@ -1,61 +1,61 @@
 using DG.Tweening;
 using UnityEngine;
-public class Rotate : MonoBehaviour {
+public class Rotate : MonoBehaviour
+{
 
-	[SerializeField] Transform plot;
-	[SerializeField] bool left;
-	[SerializeField] float smooth = 0.3f;
-	[SerializeField] float angle = 15;
+    [SerializeField] Transform plot;
+    [SerializeField] Rigidbody plotRb;
 
-	[SerializeField] AudioClip buttonSound;
-	Animator anim;
-	AudioSource _audioSource;
-	private void Start()
-	{
-		_audioSource = this.gameObject.GetComponent<AudioSource> ();
-		anim = GetComponentInParent<Animator>();
-	}
-	
+    [SerializeField] bool left;
+    [SerializeField] float smooth = 0.3f;
+    [SerializeField] float angle = 15;
 
+    [SerializeField] AudioClip buttonSound;
+    Animator anim;
+    AudioSource _audioSource;
+    private void Start()
+    {
+        _audioSource = this.gameObject.GetComponent<AudioSource>();
+        anim = GetComponentInParent<Animator>();
+    }
 
-	
-
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.tag == "Dragon" && (anim.GetBool("Press")))
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Dragon" && (anim.GetBool("Press")))
         {
-			_audioSource.Play();
-			anim.SetBool("Press", false);
-		}
-			
-	}
+            _audioSource.Play();
+            anim.SetBool("Press", false);
+        }
+    }
 
-
-	/*private void OnTriggerStay () =>
+    /*private void OnTriggerStay () =>
 		 plot.DORotate (new Vector3 (0, plot.rotation.eulerAngles.y +
 			(angle * (left ? -1 : 1)), 0), smooth);*/
-	public void OnTriggerStay(Collider other)
+    public void OnTriggerStay(Collider other)
     {
-
-		if (other.tag == "Dragon")
-		{
-			if (other.gameObject.GetComponent<DragonMove>().anim.GetBool("Finish"))
-			{
-				if (!anim.GetBool("Press"))
-                {
-					anim.SetBool("Press", true);
-					_audioSource.Play();
-				}
-				plot.DORotate(new Vector3(0, plot.rotation.eulerAngles.y + (angle * (left ? -1 : 1)), 0), smooth);
-			}
-			else
+        if (other.tag == "Dragon")
+        {
+            if (other.gameObject.GetComponent<DragonMove>().anim.GetBool("Finish"))
             {
-				if (anim.GetBool("Press"))
-				{
-					anim.SetBool("Press", false);
-					_audioSource.Play();
-				}
-			}
-		}
-	}
+                if (!anim.GetBool("Press"))
+                {
+                    anim.SetBool("Press", true);
+                    _audioSource.Play();
+                }
+                Rotation();
+            }
+            else
+            {
+                if (anim.GetBool("Press"))
+                {
+                    anim.SetBool("Press", false);
+                    _audioSource.Play();
+                }
+            }
+        }
+    }
+    void Rotation()
+    {
+        plotRb.DORotate(new Vector3(0, plot.rotation.eulerAngles.y + (angle * (left ? -1 : 1)), 0), smooth);
+    }
 }
