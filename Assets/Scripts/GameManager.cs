6f46695d6711490e98bf963d6f8849a0d362d1bb
Dmitriy <Dimamakarov1941@yandex.ru>
2020-12-20 18:17:38 +0300

    add menu button (start and restart)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+public class GameManager : MonoBehaviour
+{
 
-public class GameManager : MonoBehaviour {
-	[SerializeField] KeyCode ReloadButton = KeyCode.R;
-	private void Update () {
-		if (Input.GetKeyDown (ReloadButton))
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
-	}
+    [SerializeField] DragonMove move;
+    [SerializeField] GameObject startButton;
 
+    public void PressStart()
+    {
+        move.enabled = true;
+        // disable kinematic
+        move.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        startButton.transform.DOMoveY(20, 5);
+    }
+    IEnumerator DeleteStartButton()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(startButton);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
