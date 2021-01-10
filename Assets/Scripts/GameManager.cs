bec94a0ff4db4fc90d755c09f6d5a8dba0d8a8b1
Dmitriy <Dimamakarov1941@yandex.ru>
2021-01-10 14:11:01 +0300

    fix desing of menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] DragonMove DragonMove;
     [SerializeField] GameObject startButton;
     [SerializeField] GameObject dragonButton;
-
-    [SerializeField] SpriteRenderer logo;
     public void PressStart()
     {
         DragonMove.enabled = true;
@@ -21,19 +19,12 @@ public class GameManager : MonoBehaviour
         }
         startButton.transform.DOScale(new Vector3(0.001f, 0.001f, 0.001f), 1f);
         dragonButton.transform.DOScale(new Vector3(0.001f, 0.001f, 0.001f), 1f);
-        StartCoroutine(ChangeColorOfLogo());
-    }
-    IEnumerator ChangeColorOfLogo()
-    {
-        yield return new WaitForSeconds(1f);
-        logo.DOColor(new Color(logo.color.r, logo.color.g, logo.color.g, 0), 20);
     }
     IEnumerator DeleteStartButton()
     {
         yield return new WaitForSeconds(20f);
         // destroy our objects
         Destroy(startButton);
-        Destroy(logo.gameObject);
     }
     public void Restart()
     {
