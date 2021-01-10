75a110d4bc7911fe1fbc64ad04096d86881a2120
Dmitriy <Dimamakarov1941@yandex.ru>
2021-01-10 16:13:39 +0300

    fix apples

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,24 +7,12 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] DragonMove DragonMove;
     [SerializeField] GameObject startButton;
-    [SerializeField] GameObject dragonButton;
     public void PressStart()
     {
         DragonMove.enabled = true;
-        // disable kinematic
-        // DragonMove.gameObject.GetComponent<Rigidbody>().useGravity = true;
         if (startButton == null)
-        {
             startButton = GameObject.Find("Menu/StartButton");
-        }
         startButton.transform.DOScale(new Vector3(0.001f, 0.001f, 0.001f), 1f);
-        dragonButton.transform.DOScale(new Vector3(0.001f, 0.001f, 0.001f), 1f);
-    }
-    IEnumerator DeleteStartButton()
-    {
-        yield return new WaitForSeconds(20f);
-        // destroy our objects
-        Destroy(startButton);
     }
     public void Restart()
     {
