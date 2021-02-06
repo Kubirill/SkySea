b0efcb5f2e95dfc8e7b8e72665a4afb15715c430
Dmitriy <Dimamakarov1941@yandex.ru>
2021-02-06 12:23:11 +0300

    add focus press

--- Assets/Scripts/Menu/GameManager.cs
+++ Assets/Scripts/Menu/GameManager.cs
@@ -7,16 +7,43 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] DragonMove DragonMove;
     [SerializeField] GameObject startButton;
+    string btnEvent;
+    // timers for press on long focus
+    public void Focus(string buttonEvent)
+    {
+        btnEvent = buttonEvent;
+        print("Focus Button: " + btnEvent);
+        StartCoroutine(Press());
+    }
+    public void UnFocus()
+    {
+        print("Defocus Button");
+        btnEvent = "break";
+    }
+    IEnumerator Press()
+    {
+        // if we look at button 2s, we press button
+        yield return new WaitForSeconds(2f);
+        if (btnEvent == "start")
+            PressStart();
+        else if (btnEvent == "restart")
+            Restart();
+    }
     public void PressStart()
     {
+        print("Start game 🏁");
+        // we start game
         DragonMove.enabled = true;
         DragonMove.gameObject.GetComponent<Rigidbody>().useGravity = true;
         if (startButton == null)
             startButton = GameObject.Find("Menu/StartButton");
+        // delete button
         startButton.transform.DOScale(new Vector3(0.001f, 0.001f, 0.001f), 1f);
+        Destroy(startButton, 2f);
     }
     public void Restart()
     {
+        print("Restart game! 🔁");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
